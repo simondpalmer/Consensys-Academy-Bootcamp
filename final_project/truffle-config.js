a575b4542f8bca30ff6ca86f5304c94b284b64d4
Simon Palmer <palmer.simond@gmail.com>
2020-11-29 13:56:42 -0800

    Rinkeby network settings updated and Contract address added

--- final_project/truffle-config.js
+++ final_project/truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+require('dotenv').config();
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env["NEMONIC"];
+var tokenkey = process.env["ENDPOINT_KEY"];
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,11 +13,20 @@ module.exports = {
       host: "127.0.0.1",
       port: "8545",
       network_id: "*"
+    },
+    rinkeby: {
+      host: "127.0.0.1",
+      provider: function() {
+        return new HDWalletProvider( mnemonic, "https://rinkeby.infura.io/v3/" + tokenkey,1)
+      },
+      network_id: 4,
+      gas: 4612388,
+      gasPrice: 10000000000
     }
   },
   compilers: {
     solc: {
-      version: ">=0.4.21 <0.7.0", // A version or constraint - Ex. "^0.5.0"
+      version: "^0.6.0", // A version or constraint - Ex. "^0.5.0"
                          // Can also be set to "native" to use a native solc
     }
   }
